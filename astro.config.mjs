b85880aec77e6487697b6219dcf960c73bc8d328
Mr. P Casey <pcasey_boynecs@TCH12BoyneCS.boynecs.local>
2025-11-26 14:54:04 +0000

    Add tailwind config map

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // We removed the 'base' and 'site' lines so it works at the root of Netlify
 });
