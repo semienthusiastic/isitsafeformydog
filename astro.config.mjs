b76bf6b126e480dec56683160d0c7405e5e65715
Mr. P Casey <pcasey_boynecs@TCH12BoyneCS.boynecs.local>
2025-11-26 14:06:10 +0000

    Fix config for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  // We removed the 'base' and 'site' lines so it works at the root of Netlify
+});
